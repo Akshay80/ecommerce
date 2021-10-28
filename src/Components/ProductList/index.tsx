60ba0a9f34c7f331f7644866fcaa954b3c8b904b
Akshay Bisht <akshay.bisht@cynoteck.com>
2021-10-28 12:50:58 +0530

    My changes

--- src/Components/ProductList/index.tsx
+++ src/Components/ProductList/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./products.css";
 import { useEffect } from "react";
+import Rate from "@material-ui/lab/Rating";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 interface Ratings {
   rate: number;
@@ -38,6 +40,7 @@ interface Props {
 export const ProductList: React.FC<Props> = ({ category }) => {
   const [product, setProduct] = useState<Products[]>([]);
   const [cartItem, setCart] = useState<CartProducts | undefined>(undefined);
+  const [loading, setLoading] = useState(false);
 
   function getProducts() {
     let url: string;
@@ -46,11 +49,15 @@ export const ProductList: React.FC<Props> = ({ category }) => {
     } else {
       url = "https://fakestoreapi.com/products";
     }
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        setProduct(res);
-      });
+    setLoading(true);
+    setTimeout(() => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          setProduct(res);
+        });
+      setLoading(false);
+    }, 1000);
   }
 
   useEffect(() => {
@@ -76,7 +83,7 @@ export const ProductList: React.FC<Props> = ({ category }) => {
           </div>
 
           {product.map((items, index) => (
-            <div className="col-md-6 col-lg-4">
+            <div className="col-md-6 col-lg-4" key={index}>
               <div className="card-box">
                 <div className="card-thumbnail">
                   <img src={items.image} className="img-fluid" alt="" />
@@ -86,18 +93,19 @@ export const ProductList: React.FC<Props> = ({ category }) => {
                     {items.title}
                   </a>
                 </h3>
-                <p className="text-secondary">{items.description}</p>
-                <p className="text-secondary fw-bold">Price: ${items.price}</p>
+                <p
+                  className="text-secondary fw-bold mt-3"
+                  style={{ fontSize: 24 }}
+                >
+                  ${items.price}
+                </p>
                 <div className="text-center">
-                  <a
-                    href="/"
-                    className="btn btn-sm btn-success text-center border-none me-3"
-                  >
+                  <button className="btn btn-sm btn-success text-center outline-none me-3">
                     Add to Cart <i className="fa fa-shopping-cart"></i>
-                  </a>
+                  </button>
 
                   <button
-                    className="btn btn-sm btn-primary text-center"
+                    className="btn btn-sm btn-primary text-center outline-none"
                     data-bs-toggle="modal"
                     data-bs-target="#mymodal"
                     onClick={(event: NewType) => {
@@ -112,6 +120,9 @@ export const ProductList: React.FC<Props> = ({ category }) => {
           ))}
         </div>
       </div>
+      <div className="text-center mb-5">
+        {loading && <CircularProgress color="primary"></CircularProgress>}
+      </div>
 
       <div
         className="modal fade"
@@ -120,46 +131,60 @@ export const ProductList: React.FC<Props> = ({ category }) => {
         aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog" role="document">
+        <div className="modal-dialog modal-lg" role="document">
           <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Modal title
-              </h5>
+            <div className="modal-header border-0">
               <button
                 type="button"
                 className="close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
+                style={{
+                  marginLeft: "auto",
+                  border: "none",
+                  backgroundColor: "transparent",
+                  fontSize: 30,
+                }}
               >
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div className="modal-body">
-              <img
-                src={cartItem?.image}
-                alt="cart_item"
-                width="150px"
-                height="150px"
-              />
-              <br />
-              {cartItem?.title}
-              <br />
-              {cartItem?.price}
-              <br />
-              {cartItem?.description}
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 text-center">
+                    <img
+                      src={cartItem?.image}
+                      alt="cart_item"
+                      width="200px"
+                      height="200px"
+                    />
+                  </div>
+
+                  <div className="col-md-6">
+                    <div className="title">{cartItem?.title}</div>
+                    <div className="category mt-2">
+                      CATEGORIES: {cartItem?.category}
+                    </div>
+                    <div className="price mt-2">${cartItem?.price}</div>
+                    <div className="d-inline-flex">
+                      <Rate name="read-only" value={4} readOnly />{" "}
+                      <span className="count mt-1">
+                        ( {cartItem?.rating?.count} Reviews )
+                      </span>
+                    </div>
+
+                    <div className="description mt-3">
+                      {cartItem?.description}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-              <button type="button" className="btn btn-primary">
-                Save changes
+            <div className="modal-footer border-0">
+              <button className="btn btn-sm btn-success text-center border-0 outline-none me-3">
+                Add to Cart <i className="fa fa-shopping-cart"></i>
               </button>
             </div>
           </div>
